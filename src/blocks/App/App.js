d143679a8bd6ff7e12fabb86ec1ff0ff0c890054
Danil Kosenko <detrop.1995@gmail.com>
2021-11-14 11:23:33 +0300

    Set up project to its initial state

---
+++ src/blocks/App/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './App.module.scss';
+
+function App() {
+    return <div className={styles.container}>Initial state of the project</div>;
+}
+
+export default App;
