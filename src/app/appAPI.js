880667a519b4bf39f19d6eadf529b1d764e1cbc4
Danil Kosenko <detrop.1995@gmail.com>
2022-03-28 01:59:08 +0300

    fix: security fixes

--- src/app/appAPI.js
+++ src/app/appAPI.js
@@ -1,8 +1,5 @@
 import axios from 'axios';
 
 export function fetchUsers() {
-    return axios.get(
-        'https://stoplight.io/mocks/kode-education/trainee-test/25143926/users',
-        { responseType: 'json' },
-    );
+    return axios.get(process.env.REACT_APP_API_URI, { responseType: 'json' });
 }
